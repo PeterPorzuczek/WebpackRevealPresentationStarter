ae2afb546302e343065ec7dcd37338d098996dcd
Peter <Peter@mBp8qwRwro9Wq.local>
2018-03-11 21:52:46 +0100

    image loading fixed

--- src/slides/presentations/example/team/index.js
+++ src/slides/presentations/example/team/index.js
@@ -1,4 +1,5 @@
 import profile from './profile';
+import profileImage from '../../../../lib/assets/images/example.png';
 
 const slide = (id) => 
 `
@@ -11,7 +12,7 @@ const slide = (id) =>
         <div class="content">
             ${profile(  'Peter', 
                         'Intern', 
-                        './lib/assets/images/example.png', 
+                        profileImage, 
                         'Profile 1', 
                         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
                         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
@@ -19,7 +20,7 @@ const slide = (id) =>
                     )}
             ${profile(  'Oskar', 
                         'Intern', 
-                        './lib/assets/images/example.png', 
+                        profileImage, 
                         'Profile 2', 
                         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
                         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
